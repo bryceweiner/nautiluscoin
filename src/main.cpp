1961a1e39118009aaf02c3da4856f5a45b4bf829
xploited <xploited.ca@gmail.com>
2014-04-28 00:45:29 -0700

    typo

--- src/main.cpp
+++ src/main.cpp
@@ -1152,7 +1152,7 @@ unsigned int ComputeMinWork(unsigned int nBase, int64_t nTime)
             // Maximum 10% adjustment...
             bnResult = (bnResult * 110) / 100;
             // ... in best-case exactly 4-times-normal target time
-            nTime -= nTargetTimespanRe*4;
+            nTime -= nTargetTimespan*4;
     }
     if (bnResult > bnLimit)
         bnResult = bnLimit;
